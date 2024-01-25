6be88fdd68a3d35b35288e097ae20f13873083a0
Daniel Guerrero <danxguerrero@google.com>
2024-01-25 14:51:04 -0500

    Integrated Auth0 for user login

--- index.js
+++ index.js
@@ -4,7 +4,24 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const { PORT = 3000 } = process.env;
+const { auth } = require('express-openid-connect');
 // TODO - require express-openid-connect and destructure auth from it
+
+const {
+  AUTH0_SECRET, // generate one by using: `openssl rand -base64 32`
+  AUTH0_AUDIENCE,
+  AUTH0_CLIENT_ID,
+  AUTH0_BASE_URL,
+  } = process.env;
+
+  const config = {
+  authRequired: true, // this is different from the documentation
+  auth0Logout: true,
+  secret: AUTH0_SECRET,
+  baseURL: AUTH0_AUDIENCE,
+  clientID: AUTH0_CLIENT_ID,
+  issuerBaseURL: AUTH0_BASE_URL,
+};
 
 const { User, Cupcake } = require('./db');
 
@@ -13,6 +30,7 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(auth(config));
 
 /* *********** YOUR CODE HERE *********** */
 // follow the module instructions: destructure config environment variables from process.env
@@ -20,6 +38,20 @@ app.use(express.urlencoded({extended:true}));
   // define the config object
   // attach Auth0 OIDC auth router
   // create a GET / route handler that sends back Logged in or Logged out
+
+app.get('/', async(req, res, next) => {
+  try {
+    console.log(req.oidc.user)
+    res.send(`<h1 style='text-align:center'>My Web App, Inc.</h1>
+              <h1>Welcome, ${req.oidc.user.name}</h1>
+              <p>username: ${req.oidc.user.nickname}</p>
+              <p>${req.oidc.user.email}</p>
+              <img src='${req.oidc.user.picture}'>`)
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+})
 
 app.get('/cupcakes', async (req, res, next) => {
   try {
